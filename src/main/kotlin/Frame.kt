e9c5f5f0fb6f19c911e1a3ffe96157b3c909cc19
Roshan Stephen <roshanstephen4u@gmail.com>
2021-05-13 15:08:35 +0530

    testOpenFrameScore
    succeeded

--- src/main/kotlin/Frame.kt
+++ src/main/kotlin/Frame.kt
@@ -2,10 +2,12 @@ class Frame {
 
     private var score : Int = 0;
     fun addScore(score : Int)  {
-        this.score = score
+        this.score += score
     }
 
     fun getscore() : Int{
         return score
     }
+
+
 }
