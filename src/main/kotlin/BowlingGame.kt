2d94219fbd7805ad9ccb7dc8d6c04798564e1996
Roshan Stephen <roshanstephen4u@gmail.com>
2021-05-13 17:44:43 +0530

    final commit
    added utility method to parse string.
    final refactoring

--- src/main/kotlin/BowlingGame.kt
+++ src/main/kotlin/BowlingGame.kt
@@ -10,7 +10,7 @@ class BowlingGame {
 
     fun getTotalScore(): Int {
         var grandTotal  = 0
-        scoreposition = 0
+        var scoreposition = 0
         for (i in 1..20 step 2){
             var frame = Frame(scores, scoreposition)
             grandTotal += frame.getscore()
@@ -18,5 +18,47 @@ class BowlingGame {
         }
         return grandTotal
     }
+
+
+    /**
+     * Utility method to parse users string into score card.
+     * below are some of the score string can be use to enter while
+     * run the app
+     *
+     * ex : X X X X X X X X X X X X
+     * ex : 9- 9- 9- 9- 9- 9- 9- 9- 9- 9-
+     * ex : 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5
+     */
+
+    fun parseDigitFrom(str: String): ArrayList<Int> {
+        var list = ArrayList<Int>()
+        var strwithnoWhitespace = str.replace("\\s".toRegex(), "")
+        var currentIndex = 0;
+        for (element in strwithnoWhitespace) {
+            if(element.equals('X', true)){
+                list.add(10)
+            } else if(element.equals('/')){
+                list.add(10-(list.get(currentIndex-1)))
+            }else if(element.equals('-')){
+                list.add(0)
+            }else{
+                list.add(element.digitToInt())
+            }
+            currentIndex++
+        }
+        return list;
+    }
+
+}
+
+
+fun main(args : Array<String>) {
+
+    println("enter valid sequence of Score")
+    val scoreInput = readLine()!!
+    var bowlingGame = BowlingGame()
+    var list = bowlingGame.parseDigitFrom(scoreInput)
+    list.forEach { bowlingGame.addScore(it) }
+    println("Total Score is : "+bowlingGame.getTotalScore())
 
 }
