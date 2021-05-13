1534fbbbdfef1f1138cbfee064f27c1d1fcf52d3
Roshan Stephen <roshanstephen4u@gmail.com>
2021-05-13 17:19:44 +0530

    bowlingGame test cases are all succeeded

--- src/test/kotlin/BowlingGameTest.kt
+++ src/test/kotlin/BowlingGameTest.kt
@@ -5,6 +5,7 @@ import kotlin.test.assertEquals
 
 class BowlingGameTest {
     var bowlingGame : BowlingGame? = null
+
     @BeforeEach
     internal fun setUp() {
         bowlingGame = BowlingGame()
@@ -43,5 +44,37 @@ class BowlingGameTest {
             bowlingGame?.addScore(0)
         }
         assertEquals(26, bowlingGame?.getTotalScore())
+    }
+
+    @Test
+    internal fun testScoresWithOpenFrames() {
+        bowlingGame?.addScore(4)
+        bowlingGame?.addScore(2)
+        bowlingGame?.addScore(5)
+        bowlingGame?.addScore(1)
+        for(i in 1..16){
+            bowlingGame?.addScore(0)
+        }
+        assertEquals(12, bowlingGame?.getTotalScore())
+    }
+
+    @Test
+    internal fun testScorewithLastFrameSpare() {
+        for(i in 1..18)
+            bowlingGame?.addScore(1)
+        bowlingGame?.addScore(5)
+        bowlingGame?.addScore(5)
+        bowlingGame?.addScore(3)
+        assertEquals(31, bowlingGame?.getTotalScore())
+    }
+
+    @Test
+    internal fun testScoreWithlastFrameStrike() {
+        for(i in 1..18)
+            bowlingGame?.addScore(1)
+        bowlingGame?.addScore(10)
+        bowlingGame?.addScore(5)
+        bowlingGame?.addScore(3)
+        assertEquals(36, bowlingGame?.getTotalScore())
     }
 }
