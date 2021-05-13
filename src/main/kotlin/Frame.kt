498df2c9f72030b8cac48adb9f1e4d635598958d
Roshan Stephen <roshanstephen4u@gmail.com>
2021-05-13 15:31:48 +0530

    testStrikeFrame
    succeeded

--- src/main/kotlin/Frame.kt
+++ src/main/kotlin/Frame.kt
@@ -1,24 +1,32 @@
 class Frame {
 
-    var rollScore : Int = 0 ;
+    var rollScore: Int = 0;
 
-    private var score : Int = 0;
+    private var score: Int = 0;
 
-    fun addScore(score : Int)  {
+    fun addScore(score: Int) {
         this.score += score
     }
 
-    fun addScore(score1 : Int, score2 : Int, score3 : Int)  {
-       if(isSpareScore(score1, score2))
-           this.score = 10 + score3
+    fun addScore(score1: Int, score2: Int, score3: Int) {
+        if (isStrikeScore(score1))
+            this.score = 10 +score2+score3
+        if (isSpareScore(score1, score2))
+            this.score = 10 + score3
     }
 
-    fun getscore() : Int{
+    fun getscore(): Int {
         return score
     }
 
-    fun isSpareScore(scoreFirst : Int, scoreSecond : Int) : Boolean{
-        if((scoreFirst+scoreSecond) == 10)
+    fun isSpareScore(scoreFirst: Int, scoreSecond: Int): Boolean {
+        if ((scoreFirst + scoreSecond) == 10)
+            return true
+        return false
+    }
+
+    fun isStrikeScore(scoreFirst: Int): Boolean {
+        if (scoreFirst == 10)
             return true
         return false
     }
